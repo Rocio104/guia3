fcebe73efd8cee075b04a9f17f8a1420d13ca4e3
Rocio104 <rociominero07@gmail.com>
2022-04-03 12:06:29 -0600

    segundaparte

---
+++ src/ejercicios/factura.java
@@ -0,0 +1,28 @@
+
+package ejercicios;
+
+import java.text.DecimalFormat;
+import java.text.NumberFormat;
+import java.util.Scanner;
+
+
+public class factura {
+    public static void main (String [] args) {
+        NumberFormat formato = new DecimalFormat("#0.00");
+        
+        Scanner leer = new Scanner (System.in);
+        facturaElectrica objeto = new facturaElectrica();
+        
+        System.out.println("Ingrese su N° de cuenta");
+        objeto.setNcuenta(leer.nextDouble());
+        
+        System.out.println("Numero de cuenta: " +objeto.getNcuenta());
+        System.out.println("Lectura actual del medidor: "+objeto.getLecturaActualM() + ("kWh"));
+        System.out.println("Lectura anterior del medidor: "+objeto.getLecturaAnteriorM()+("kWh"));
+        System.out.println("Su consumo mensual es: " +objeto.consumo()+("kWh"));
+        System.out.println("Su consumo diario es: " + formato.format(objeto.consumoD())+("kWh"));
+        System.out.println("El valor de factura es: " +objeto.ValorFac());
+        
+    }
+    
+}
